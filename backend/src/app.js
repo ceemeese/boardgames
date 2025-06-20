ecb0a08487a8776bb5f675b4c74bbd496b36fdf6
ceemeese <cmsalvador92@gmail.com>
2025-06-20 19:53:39 +0200

    Add config Prometheus&Grafana, update workflow to build latest version, update compose to take image from DockerHub

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors')
 const config = require('./config/configuration');
+const promClient = require('prom-client');
+const { httpRequestDurationSeconds, httpRequestsTotal, inFlightRequests } = require('./config/metrics');
+
 
 
 const boardgames = require ('./route/boardgames');
@@ -9,18 +12,57 @@ const users = require ('./route/users');
 const gamesUsers = require ('./route/gamesUsers');
 
 
-
-
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// Recoge métricas por defecto
+promClient.collectDefaultMetrics();
+// Recoger las métricas configuradas en config/metrics
+app.use((req, res, next) => {
+    // Anota la request en vuelo
+    inFlightRequests.inc();
+ 
+    // Inicia un timer para comenzar a calcular la duración de la request
+    const end = httpRequestDurationSeconds.startTimer();
+ 
+    const method = req.method;
+    const path = req.route ? req.route.path : req.path; 
+ 
+    // Cada vez que una request termina, se recogen las métricas configuradas
+    res.on('finish', () => {
+        const statusCode = res.statusCode;
+ 
+        // Captura la duración de la petición
+        end({ method, path, code: statusCode }); 
+        // Incrementa el contador de peticiones HTTP
+        httpRequestsTotal.inc({
+            code: statusCode,
+            method: method.toLowerCase(),
+            path: path
+        });
+ 
+        // Decrementa el contador de peticiones en vuelo
+        inFlightRequests.dec();
+    });
+ 
+    next();
+});
+
+// Define el endpoint que usará prometheus para recoger las métricas
+app.get('/metrics', async(req, res) => {
+    try {
+        res.set('Content-Type', promClient.register.contentType);
+        res.end(await promClient.register.metrics());
+    } catch (error) {
+        res.status(500).end(error);
+    }
+});
 
 app.use('/', boardgames)
 app.use('/', games)
 app.use('/', users)
 app.use('/', gamesUsers)
-
-
 
 app.listen(config.service.port , () => {
     console.log(`Backend iniciado correctamente por puerto ${config.service.port}`);
